b6cb8f90f24c098956aa20aed8c94ad975d4a454
lcxznpy <80241815+lcxznpy@users.noreply.github.com>
2024-01-10 23:39:17 +0800

    Update 2219.js

--- .github/scripts/2219.js
+++ .github/scripts/2219.js
@@ -33,25 +33,44 @@ async function run() {
 
     const projectsToAssociate = [];
 
-    for (const assignee of assignees) {
-      const teams = await octokit.rest.teams.list({
+    const teams = await octokit.rest.teams.list({
         org: "lcxznpy-test",  // 替换为你的组织名
       });
-      console.log("成功获得team信息",teams);
-      const team = teams.data.find((t) => t.members.some((m) => m.login === assignee.login));
-      
-      if (team && projectMapping[team.slug]) {
-        projectsToAssociate.push(projectMapping[team.slug]);
-        console.log("成功push一个信息");
+    for(const team_data of teams.data){
+      const team_member = await octokit.rest.teams.listMembersInOrg({
+            org: "lcxznpy-test",
+            team_slug: team_data.slug,
+          });
+      for(const assignee of assignees){
+        if(team_member.find((m) => m.login === assignee.login)){
+          if(projectMapping[team_data.slug]){
+            projectsToAssociate.push(projectMapping[team.slug]);
+            console.log("成功push一个信息");
+          }
+        }
+          
       }
     }
+    // for (const assignee of assignees) {
+    //   const teams = await octokit.rest.teams.list({
+    //     org: "lcxznpy-test",  // 替换为你的组织名
+    //   });
+    //   console.log("成功获得team信息",teams);
+    //   const team = teams.data.find((t) => t.members.some((m) => m.login === assignee.login));
+      
+    //   if (team && projectMapping[team.slug]) {
+        // projectsToAssociate.push(projectMapping[team.slug]);
+        // console.log("成功push一个信息");
+    //   }
+    // }
 
     if (projectsToAssociate.length === 0) {
       console.log("没有组织，使用默认组织");
       projectsToAssociate.push(3); // 默认 project ID
     }
-
-    for (const projectId of projectsToAssociate) {
+    const result = Array.from(new Set(projectsToAssociate))
+    console.log(result)
+    for (const projectId of result) {
       // 将 issue 添加到项目
       console.log("开始将issue关联到项目");
       await octokit.rest.projects.createCard({
