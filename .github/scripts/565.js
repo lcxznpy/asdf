abe49b505548685278cd8107a5b5ce5e27810272
lcxznpy <80241815+lcxznpy@users.noreply.github.com>
2024-01-15 11:07:07 +0800

    Update 565.js

--- .github/scripts/565.js
+++ .github/scripts/565.js
@@ -7,7 +7,7 @@ const octokit = new Octokit({
 async function getIssueStats() {
   try{
     const { data: issues } = await octokit.issues.listForRepo({
-    owner: process.env.GITHUB_REPOSITORY_OWNER,,
+    owner: process.env.GITHUB_REPOSITORY_OWNER,
     repo: "asdf",
     state: 'all',
     per_page: 10,
